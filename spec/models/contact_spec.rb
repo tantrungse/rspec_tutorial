e54088ab201016085ec7dc6194b6fa1cca034ae1
tantrungse <tantrungse@gmail.com>
2023-11-25 10:27:34 +0700

    Testing validations

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe Contact do
+    it "is valid with a firstname, lastname and email" do
+        contact = Contact.new(
+            firstname: 'Trung',
+            lastname: 'Vo',
+            email: 'trungvo@example.com'
+        )
+        expect(contact).to be_valid
+    end
+    it "is invalid without a firstname" do
+        contact = Contact.new(firstname: nil)
+        contact.valid?
+        expect(contact.errors[:firstname]).to include("can't be blank")
+    end
+    it "is invalid without a lastname" do
+        contact = Contact.new(lastname: nil)
+        contact.valid?
+        expect(contact.errors[:lastname]).to include("can't be blank")
+    end
+    it "is invalid without an email address" do
+        contact = Contact.new(email: nil)
+        contact.valid?
+        expect(contact.errors[:email]).to include("can't be blank")
+    end
+    it "is invalid with a duplicate email address" do
+        Contact.create(
+            firstname: 'Joe',
+            lastname: 'Tester',
+            email: 'tester@example.com'
+        )
+        contact = Contact.new(
+            firstname: 'Jane',
+            lastname: 'Tester',
+            email: 'tester@example.com'
+        )
+        contact.valid?
+        expect(contact.errors[:email]).to include("has already been taken")
+    end
+    it "returns a contact's fullname as a string"
+end
