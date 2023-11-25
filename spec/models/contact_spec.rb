c46dc49b79af759e2c5481fe7b55cc6708df7f55
tantrungse <tantrungse@gmail.com>
2023-11-25 15:17:37 +0700

    Testing class methods and scopes

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -9,21 +9,25 @@ describe Contact do
         )
         expect(contact).to be_valid
     end
+
     it "is invalid without a firstname" do
         contact = Contact.new(firstname: nil)
         contact.valid?
         expect(contact.errors[:firstname]).to include("can't be blank")
     end
+
     it "is invalid without a lastname" do
         contact = Contact.new(lastname: nil)
         contact.valid?
         expect(contact.errors[:lastname]).to include("can't be blank")
     end
+
     it "is invalid without an email address" do
         contact = Contact.new(email: nil)
         contact.valid?
         expect(contact.errors[:email]).to include("can't be blank")
     end
+
     it "is invalid with a duplicate email address" do
         Contact.create(
             firstname: 'Joe',
@@ -38,6 +42,7 @@ describe Contact do
         contact.valid?
         expect(contact.errors[:email]).to include("has already been taken")
     end
+
     it "returns a contact's fullname as a string" do
         contact = Contact.new(
             firstname: 'John',
@@ -45,5 +50,24 @@ describe Contact do
             email: 'johndoe@gmail.com'
         )
         expect(contact.name).to eq 'John Doe'
+    end
+    
+    it "returns a sorted array of results that match" do
+        smith = Contact.create(
+            firstname: 'John',
+            lastname: 'Smith',
+            email: 'jsmith@example.com'
+        )
+        jones = Contact.create(
+            firstname: 'Tim',
+            lastname: 'Jones',
+            email: 'tjones@example.com'
+        )
+        johnson = Contact.create(
+            firstname: 'John',
+            lastname: 'Johnson',
+            email: 'jjohnson@example.com'
+        )
+        expect(Contact.by_letter("J")).to eq [johnson, jones]
     end
 end
